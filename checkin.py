1a1cbcdf79533c2d078ac42cc41ee0df23b83d5a
Sizisa <1327438873@qq.com>
2020-07-26 13:10:45 +0800

    up

--- checkin.py
+++ checkin.py
@@ -5,6 +5,13 @@ import requests
 
 def checkin(params):
     res = requests.post(url='https://ssl.kohsocialapp.qq.com:10001/play/h5sign', data=params, headers={'Content-Type': 'application/x-www-form-urlencoded'})
-    print(res.text)
+    
     state = res.json()
     print(state)
+    
+    if state['result']==-1:    
+        return (-1,state['returnMsg'],0)
+    if state['result']==0:
+        mag=state['data']['todayGift'].split('|')[1]
+        return(0,msg,state['data']['userTotalSign'])
+    return (999,res.text,0)
