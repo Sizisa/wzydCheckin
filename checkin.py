c759fe652b22de2e71054d0bc70cda8f7cdb3984
Sizisa <1327438873@qq.com>
2020-07-27 10:06:30 +0800

    Update checkin.py

--- checkin.py
+++ checkin.py
@@ -12,6 +12,6 @@ def checkin(params):
     if state['result']==-1:    
         return (-1,state['returnMsg'],0)
     if state['result']==0:
-        mag=state['data']['todayGift'].split('|')[1]
+        msg=state['data']['todayGift'].split('|')[1]
         return(0,msg,state['data']['userTotalSign'])
     return (999,res.text,0)
